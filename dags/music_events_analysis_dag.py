3f5e5ae2da4d6979e202d18aa7b52f393967c5c6
XiaofangChu <xfang.chu@gmail.com>
2020-07-08 00:14:28 -0700

    data pipeline with Airflow

---
+++ dags/music_events_analysis_dag.py
@@ -0,0 +1,137 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+start_date = datetime(2018, 11, 1)
+
+default_args = {
+    'owner': 'xiaofanc',
+    'start_date': datetime(2018, 11, 1),
+    'end_date': datetime(2018, 11, 30),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=300),
+    'catchup': False
+}
+
+dag = DAG('music_events_analysis_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          max_active_runs=1
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+create_tables=PostgresOperator(
+    task_id='create_tables',
+    dag=dag,
+    postgres_conn_id='redshift',
+    sql='create_tables.sql'
+)
+
+# staging table from s3 to redshift
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    provide_context=True,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    region="us-west-2",
+    file_format="JSON",
+    execution_date=start_date
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    provide_context=True,
+    table="staging_songs",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    region="us-west-2",
+    data_format="JSON"
+)
+
+# transform data to fact and dimension tables in redshift
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    select_sql=SqlQueries.songplay_table_insert,
+    truncate=True
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    select_sql=SqlQueries.user_table_insert,
+    truncate=True
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songs",
+    select_sql=SqlQueries.song_table_insert,
+    truncate=True
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    select_sql=SqlQueries.artist_table_insert,
+    truncate=True
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    select_sql=SqlQueries.time_table_insert,
+    truncate=True
+)
+
+# quality check 
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+# step 1: staging
+start_operator >> [stage_songs_to_redshift, stage_events_to_redshift]
+
+# step 2: load songplay fact table
+[stage_songs_to_redshift, stage_events_to_redshift] >> load_songplays_table
+
+# step 3: load dimension tables
+load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table]
+
+# step 4: quality check
+[load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks 
+
+# end operator
+run_quality_checks >> end_operator
+
---
+++ plugins/operators/load_dimension.py
@@ -0,0 +1,49 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+    insert_table = """
+        INSERT INTO {table}
+        {select_query}
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 select_query="",
+                 truncate="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_query = select_query
+        self.truncate = truncate
+
+    def execute(self, context):
+        """
+        transform staging tables into dimension table
+        
+        parameters:
+            - redshift_conn_id: redshift cluster connection
+            - table: dimension table name
+            - truncate: clean table before loading
+            - select_query: select query from SqlQueries
+        """
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        # table exists
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+            
+        self.log.info(f'Load {self.table}')
+        load_sql = LoadFactOperator.insert_table.format(
+            table = self.table,
+            select_query = self.select_query  
+        )
+        redshift.run(load_sql)
